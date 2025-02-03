e8e5eeccf14005e72ab95b632b35d571d6f21113
Ayush Rawat <ayushrawat220804@gmail.com>
2025-02-03 13:39:00 +0530

    Fix: Remove 3 lines from cpp_2023.cpp C++ code

--- cpp_dsa/cpp_2023.cpp
+++ cpp_dsa/cpp_2023.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 class rohit{
     public:
-    char name[20];
-    void yourname(){
-        std::cout << "enter you name : " ;
         std::cin >> name;
     }
     void display(){
