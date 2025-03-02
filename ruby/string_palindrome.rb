aff7c8b65999e36401c12e93ed401b290cb6dcb9
Ayush Rawat <ayushrawat220804@gmail.com>
2025-03-02 17:11:00 +0530

    Amend: Remove 3 lines from string_palindrome.rb Ruby code

--- ruby/string_palindrome.rb
+++ ruby/string_palindrome.rb
@@ -4,6 +4,3 @@ puts "enter a string"
 string = gets.chomp
 if string == string.reverse
   puts "yes the give string #{string} is palindrome"
-else
-  puts "no the given string #{string} is not palindrome"
-end
