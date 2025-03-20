53d766c69e2908409b0d10e585b3ac71f237074a
Ayush Rawat <ayushrawat220804@gmail.com>
2025-03-20 18:17:00 +0530

    Change: Update (add 1, remove 1) doublycircular.cpp C++ code

--- c/linkedlist/doublycircular.cpp
+++ c/linkedlist/doublycircular.cpp
@@ -69,7 +69,7 @@ int main(){
 	third = (struct node*)malloc(sizeof(struct node));
 	
 	head->data=10;
-	head->next=second;
+	head->next=second; /* modified */
 	head->prev=third;
 	
 	second->data=20;
