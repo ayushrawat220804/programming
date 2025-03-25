3c72183dac4579c12f5b0664c7572d90b740f7a6
Ayush Rawat <ayushrawat220804@gmail.com>
2025-03-25 22:26:00 +0530

    Review: Update (add 1, remove 1) reverse_string.rb Ruby code

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,4 +1,4 @@
 puts "programme to reverse a string"
 puts "enter a string"
 string = gets.chomp
-puts "reverse of #{string} --> #{string.reverse}"
+puts "reverse of #{string} --> #{string.reverse}" // Modified
