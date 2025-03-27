a62b1e5efdb0b9233d71eb56b4a68c70370b4b42
Ayush Rawat <ayushrawat220804@gmail.com>
2025-03-27 18:29:00 +0530

    Fix: Remove 2 lines from any_off.cpp C++ code

--- cpp_dsa/any_off.cpp
+++ cpp_dsa/any_off.cpp
@@ -10,8 +10,6 @@ int main() {
     cout << "Enter a string: ";
     getline(cin, s);
     cout << "You entered: " << s << endl;
-
-    if (any_of(s.begin(), s.end(), ::isdigit)) { // ::isdigit check the string does it contain any digit
         cout << "\nYes, the given string has some digits." << endl;
     } else {
         cout << "\nNo, the given string doesn't have any digits." << endl;
