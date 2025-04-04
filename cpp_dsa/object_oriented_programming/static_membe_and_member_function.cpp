6b8fbdf7786bd0431e818df5a7b8c55d8c64c17c
Ayush Rawat <ayushrawat220804@gmail.com>
2025-04-04 21:38:00 +0530

    Enhance: Remove 3 lines from static_membe_and_member_function.cpp C++ code

--- cpp_dsa/object_oriented_programming/static_membe_and_member_function.cpp
+++ cpp_dsa/object_oriented_programming/static_membe_and_member_function.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 // lappu sa dsa
 using namespace std;
-/* chomu ye ek comment h 
-static member and the member function
-why we use static members of a class
 we know that every object make new storage for their calculation and the operation perform but when we use but but! the other object
 is always share that member allocated storage.
 
