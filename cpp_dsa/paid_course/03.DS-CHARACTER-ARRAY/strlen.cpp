8b25d66a79703d60a60b64f53fee110798fdc825
Ayush Rawat <ayushrawat220804@gmail.com>
2025-04-28 16:58:00 +0530

    Change: Remove 2 lines from strlen.cpp C++ code

--- cpp_dsa/paid_course/03.DS-CHARACTER-ARRAY/strlen.cpp
+++ cpp_dsa/paid_course/03.DS-CHARACTER-ARRAY/strlen.cpp
@@ -8,8 +8,6 @@ int main(){
 	cin >> str1>>str2;
 	int length1 = strlen(str1);
 	int length2 = strlen(str2);
-	cout << "length of string 1 : " << length1 << endl;
-	cout << "length of string 2 : " << length2;
 	
 	return 0;
 }
